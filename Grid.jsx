eca8cb525b38c8fea3c48ad0b5868a4bee06c19d
Arup Kumar Behera <76170236+ArupGhoost@users.noreply.github.com>
2021-04-19 21:00:37 +0530

    Add files via upload

---
+++ Grid.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Grid.scss'
+
+const Grid = () => {
+    return (
+        <>
+         <section className='grid'>
+             <div className='card1'> 1 </div>
+             <div className='card2'> 2 </div>
+             <div className='card3'> 3 </div>
+             <div className='card4'> 4 </div>
+             <div className='card5'> 5 </div>
+             <div className='card6'> 6 </div>
+             <div className='card7'> 7 </div>
+             <div className='card8'> 8 </div>
+             <div className='card9'> 9 </div>
+             <div className='card10'> 10 </div>
+         </section>   
+        </>
+    )
+}
+
+export default Grid;
